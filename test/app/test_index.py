2934ea9a265526b083678200411e77900f903d73
Chris Devereux <devereux.chris@gmail.com>
2022-04-26 16:18:20 +0100

    format testcase

--- test/app/test_index.py
+++ test/app/test_index.py
@@ -1,10 +1,10 @@
-from django.urls import resolve
-from django.test import TestCase
 from django.http import HttpRequest
+from django.test import TestCase
+from django.urls import resolve
 
 
 class HomePageTest(TestCase):
     def test_returns_root_page(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
 
         self.assertEqual(response.status_code, 200)
